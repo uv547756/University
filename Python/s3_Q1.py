1cb2dfd74fc3357fde7dbe2102601b1e7c68cf54
Komi <uv47756@gmail.com>
2024-03-05 23:33:25 +0530

    Added set 3 problems

---
+++ Python/s3_Q1.py
@@ -0,0 +1,5 @@
+# Program to accept a list from user and print the alternate element of list
+
+lis = list(input("Enter elements in list: "))
+alt_lis = lis[::2]
+print("ALternate element list: ", alt_lis)
