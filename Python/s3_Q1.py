8c3e1d8662d737e27cc9b0ddaec6f7a9f691a388
Komi <59288856+uv547756@users.noreply.github.com>
2024-03-13 15:35:43 +0530

    Update s3_Q1.py

--- Python/s3_Q1.py
+++ Python/s3_Q1.py
@@ -3,3 +3,8 @@
 lis = list(input("Enter elements in list: "))
 alt_lis = lis[::2]
 print("ALternate element list: ", alt_lis)
+
+
+
+
+# if you are reading this, then please know that whatever happened, I wish it didn't and it's all a big misunderstanding and I have failed to explain myself every step of the way. I do not think of you the way everyone is assuming, never have never will. I only admired you. this is my final reply and I won't try again.
