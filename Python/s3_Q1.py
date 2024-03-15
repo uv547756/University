3e7c0018baad7268c7e36753b800811183d36c70
Komi <59288856+uv547756@users.noreply.github.com>
2024-03-15 12:27:47 +0530

    Update s3_Q1.py

--- Python/s3_Q1.py
+++ Python/s3_Q1.py
@@ -7,4 +7,4 @@ print("ALternate element list: ", alt_lis)
 
 
 
-# if you are reading this, then please know that whatever happened, I wish it didn't and it's all a big misunderstanding and I have failed to explain myself every step of the way. I do not think of you the way everyone is assuming, never have never will. I only admired you. this is my final reply and I won't try again.
+# if you are reading this, then please know that whatever happened, I wish it didn't and it's all a big misunderstanding and I have failed to explain myself every step of the way. I do not think of you the way everyone is assuming, never have never will. I only admired you and I just wish to have 1 last conversation face to face (if willing) this is my final reply and I won't try again.
